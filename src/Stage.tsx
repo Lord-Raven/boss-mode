ef97df757804e72e851bc2a5172c0b4271ac50d9
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2026-07-06 10:08:29 -0400

    Trying a different instructional format.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -183,7 +183,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         }
 
         if (stageDirections.length > 0) {
-            stageDirections = `<SpecialInstructions>${stageDirections}</SpecialInstructions>`;
+            stageDirections = `<AdditionalInstructionsAndGuidance><Description>These additional instructions and guidance are critical to the narrative; incorporate narrative guidance and honor any instructions or rules described herein.</Description>${stageDirections}</AdditionalInstructionsAndGuidance>`;
             console.log(`Sending stage directions:\n${stageDirections}`);
         }
 
